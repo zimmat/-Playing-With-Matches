e28f7f5708f521c7ef242abfecc4286a37214f64
zimmat <zimkhitha@projectcodex.co>
2016-02-29 12:21:25 +0200

    Add these changes

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+function getRandomNumber(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var el = document.querySelector("#random");
+function upDate(){
+el.innerHTML = getRandomNumber(1,9);
+
+}
+getRandomNumber(1,9);
+upDate();
+function getRandomNumber(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var el = document.querySelector("#number-one");
+function upDate(){
+el.innerHTML = getRandomNumber(1,9);
+
+}
+getRandomNumber(1,9);
+upDate();
+function getRandomNumber(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var el = document.querySelector("#number-two");
+function upDate(){
+el.innerHTML = getRandomNumber(1,9);
+
+}
+getRandomNumber(1,9);
+upDate();
