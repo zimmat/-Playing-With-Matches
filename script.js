a5124c21e0ae9eca67d2f13879a475a39081c25e
zimmat <zimkhitha@projectcodex.co>
2016-03-14 15:33:49 +0200

    Add

--- script.js
+++ script.js
@@ -19,79 +19,79 @@ function getRandomNumber(min, max) {
         if (el[0].innerHTML === el[1].innerHTML) {
           el[0].classList.add("highlight");
           el[1].classList.add("highlight");
-          matchRowOne.innerHTML = "in row one" + " " + el[0].innerHTML + " " + "is a matching number";
+          matchRowOne.innerHTML = "In row one" + " " + el[0].innerHTML + " " + "is a matching number";
 
         }
 
         if (el[0].innerHTML === el[2].innerHTML) {
           el[0].classList.add("highlight");
           el[2].classList.add("highlight");
-          matchRowOne.innerHTML = "in row one" + " " + el[0].innerHTML + " " + "is a matching number";
+          matchRowOne.innerHTML = "In row one" + " " + el[0].innerHTML + " " + "is a matching number";
         }
 
 
         if (el[1].innerHTML === el[2].innerHTML) {
           el[1].classList.add("highlight");
           el[2].classList.add("highlight");
-          matchRowOne.innerHTML = "in row one" + " " + el[2].innerHTML + " " + "is a matching number"  ;
+          matchRowOne.innerHTML = "In row one" + " " + el[2].innerHTML + " " + "is a matching number"  ;
       }
       if (el[0].innerHTML === el[1].innerHTML === el[2].innerHTML ) {
         el[0].classList.add("highlight");
         el[1].classList.add("highlight");
         el[2].classList.add("highlight");
-        matchRowOne.innerHTML = "in row three" + " " + el[1].innerHTML + " " + "is a matching number" ;
+        matchRowOne.innerHTML = "In row three" + " " + el[1].innerHTML + " " + "is a matching number" ;
       }
 
 
         if (el[3].innerHTML === el[5].innerHTML) {
           el[3].classList.add("highlight");
           el[5].classList.add("highlight");
-          matchRowTwo.innerHTML = "in row two" + " " + el[3].innerHTML + " " + "is a matching" ;
+          matchRowTwo.innerHTML = "In row two" + " " + el[3].innerHTML + " " + "is a matching" ;
 
         }
 
         if (el[3].innerHTML === el[4].innerHTML) {
           el[3].classList.add("highlight");
           el[4].classList.add("highlight");
-          matchRowTwo.innerHTML = "in row two" + " " + el[4].innerHTML + " " + "is a matching number"  ;
+          matchRowTwo.innerHTML = "In row two" + " " + el[4].innerHTML + " " + "is a matching number"  ;
         }
 
 
         if (el[4].innerHTML === el[5].innerHTML) {
           el[4].classList.add("highlight");
           el[5].classList.add("highlight");
-          matchRowTwo.innerHTML =  "in row two" + " " + el[5].innerHTML + " " + "is a matching number" ;
+          matchRowTwo.innerHTML =  "In row two" + " " + el[5].innerHTML + " " + "is a matching number" ;
         }
         if (el[3].innerHTML === el[4].innerHTML === el[5]) {
           el[3].classList.add("highlight");
           el[4].classList.add("highlight");
           el[5].classList.add("highlight");
-          matchRowTwo.innerHTML = "in row three" + " " + el[5].innerHTML + " " + "is a matching number" ;
+          matchRowTwo.innerHTML = "In row three" + " " + el[5].innerHTML + " " + "is a matching number" ;
         }
         if (el[6].innerHTML === el[8].innerHTML) {
           el[6].classList.add("highlight");
           el[8].classList.add("highlight");
-          matchRowThree.innerHTML = "in row three" + " " + el[6].innerHTML + " " + "is a matching number"  ;
+          matchRowThree.innerHTML = "In row three" + " " + el[6].innerHTML + " " + "is a matching number"  ;
 
         }
 
         if (el[6].innerHTML === el[7].innerHTML) {
           el[6].classList.add("highlight");
           el[7].classList.add("highlight");
-          matchRowThree.innerHTML = "in row three" + " " + el[7].innerHTML + " " + "is a matching number" ;
+          matchRowThree.innerHTML = "In row three" + " " + el[7].innerHTML + " " + "is a matching number" ;
         }
 
 
         if (el[7].innerHTML === el[8].innerHTML) {
           el[7].classList.add("highlight");
           el[8].classList.add("highlight");
-          matchRowThree.innerHTML = "in row three" + " " + el[8].innerHTML + " " + "is a matching number" ;
+          matchRowThree.innerHTML = "In row three" + " " + el[8].innerHTML + " " + "is a matching number" ;
         }
         if (el[6].innerHTML === el[7].innerHTML === el[8].innerHTML) {
           el[6].classList.add("highlight");
           el[7].classList.add("highlight");
           el[8].classList.add("highlight");
-          matchRowThree.innerHTML = "in row three" + " " + el[8].innerHTML + " " + "is a matching number" ;
+          matchRowThree.innerHTML = "In row three" + " " + el[8].innerHTML + " " + "is a matching number" ;
         }
 
       };
@@ -105,7 +105,7 @@ function getRandomNumber(min, max) {
           if (el[0].innerHTML === el[6].innerHTML) {
             el[0].classList.add("highlight");
             el[6].classList.add("highlight");
-            matchColumnOne.innerHTML = "in Column one" + " " + el[0].innerHTML + " " + "is a matching number";
+            matchColumnOne.innerHTML = "In Column one" + " " + el[0].innerHTML + " " + "is a matching number";
 
            }
 
@@ -113,169 +113,136 @@ function getRandomNumber(min, max) {
           if (el[0].innerHTML === el[3].innerHTML) {
             el[0].classList.add("highlight");
             el[3].classList.add("highlight");
-            matchColumnOne.innerHTML = "in Column one" + " " + el[0].innerHTML + " " + "is a matching number";
+            matchColumnOne.innerHTML = "In Column one" + " " + el[0].innerHTML + " " + "is a matching number";
           }
 
 //
           if (el[3].innerHTML === el[6].innerHTML) {
             el[3].classList.add("highlight");
             el[6].classList.add("highlight");
-            matchColumnOne.innerHTML = "in Column one" + " " + el[2].innerHTML + " " + "is a matching number"  ;
+            matchColumnOne.innerHTML = "In Column one" + " " + el[6].innerHTML + " " + "is a matching number"  ;
         }
 
         if (el[0].innerHTML === el[3].innerHTML === el[6].innerHTML ) {
           el[0].classList.add("highlight");
           el[3].classList.add("highlight");
           el[6].classList.add("highlight");
-          matchColumnOne.innerHTML = "in column three" + " " + el[6].innerHTML + " " + "is a matching number" ;
+          matchColumnOne.innerHTML = "In column three" + " " + el[6].innerHTML + " " + "is a matching number" ;
         }
 
           if (el[1].innerHTML === el[7].innerHTML) {
             el[1].classList.add("highlight");
             el[7].classList.add("highlight");
-            matchColumnTwo.innerHTML = "in column two" + " " + el[7].innerHTML + " " + "is a matching" ;
+            matchColumnTwo.innerHTML = "In column two" + " " + el[7].innerHTML + " " + "is a matching" ;
 
           }
 
           if (el[1].innerHTML === el[4].innerHTML) {
             el[1].classList.add("highlight");
             el[4].classList.add("highlight");
-            matchColumnTwo.innerHTML = "in column two" + " " + el[4].innerHTML + " " + "is a matching number"  ;
+            matchColumnTwo.innerHTML = "In column two" + " " + el[4].innerHTML + " " + "is a matching number"  ;
           }
 
 
           if (el[4].innerHTML === el[7].innerHTML) {
             el[4].classList.add("highlight");
             el[7].classList.add("highlight");
-            matchColumnTwo.innerHTML =  "in column two" + " " + el[6].innerHTML + " " + "is a matching number" ;
+            matchColumnTwo.innerHTML =  "In column two" + " " + el[7].innerHTML + " " + "is a matching number" ;
           }
           if (el[1].innerHTML === el[4].innerHTML === el[7]) {
             el[1].classList.add("highlight");
             el[4].classList.add("highlight");
             el[7].classList.add("highlight");
-            matchColumnTwo.innerHTML = "in column three" + " " + el[7].innerHTML + " " + "is a matching number" ;
+            matchColumnTwo.innerHTML = "In column three" + " " + el[7].innerHTML + " " + "is a matching number" ;
           }
 
 
           if (el[2].innerHTML === el[8].innerHTML) {
             el[2].classList.add("highlight");
             el[8].classList.add("highlight");
-            matchColumnThree.innerHTML = "in column three" + " " + el[1].innerHTML + " " + "is a matching number"  ;
+            matchColumnThree.innerHTML = "In column three" + " " + el[2].innerHTML + " " + "is a matching number"  ;
 
           }
 
           if (el[2].innerHTML === el[5].innerHTML) {
             el[2].classList.add("highlight");
             el[5].classList.add("highlight");
-            matchColumnThree.innerHTML = "in column three" + " " + el[7].innerHTML + " " + "is a matching number" ;
+            matchColumnThree.innerHTML = "In column three" + " " + el[5].innerHTML + " " + "is a matching number" ;
           }
 
 
           if (el[5].innerHTML === el[8].innerHTML) {
             el[5].classList.add("highlight");
             el[8].classList.add("highlight");
-            matchColumnThree.innerHTML = "in column three" + " " + el[8].innerHTML + " " + "is a matching number" ;
+            matchColumnThree.innerHTML = "In column three" + " " + el[8].innerHTML + " " + "is a matching number" ;
           }
           if (el[2].innerHTML === el[5].innerHTML === el[8].innerHTML) {
             el[2].classList.add("highlight");
             el[5].classList.add("highlight");
             el[8].classList.add("highlight");
-            matchColumnThree.innerHTML = "in Column three" + " " + el[8].innerHTML + " " + "is a matching number" ;
+            matchColumnThree.innerHTML = "In Column three" + " " + el[8].innerHTML + " " + "is a matching number" ;
           }
 
         }
-    
-        matchingColumns();
-// };
-// upDate();
 
-// //
-// // var matchingColumns = function() {
-// //   var matchColumnOne = document.getElementById("messageFour");
-// //   var matchColumnTwo = document.getElementById("messageFive");
-// //   var matchColumnThree = document.getElementById("messageSix");
-// //   if (el[0].innerHTML === el[6].innerHTML) {
-// //     el[0].classList.add("highlight");
-// //     el[6].classList.add("highlight");
-// //     matchColumnOne.innerHTML = "in row one" + " " + el[0].innerHTML + " " + "is a matching number";
-// //
-// //   }
-// //
-// //   if (el[0].innerHTML === el[3].innerHTML) {
-// //     el[0].classList.add("highlight");
-// //     el[3].classList.add("highlight");
-// //     matchColumnOne.innerHTML = "in row one" + " " + el[0].innerHTML + " " + "is a matching number";
-// //   }
-// //
-// //
-// //   if (el[3].innerHTML === el[6].innerHTML) {
-// //     el[3].classList.add("highlight");
-// //     el[6].classList.add("highlight");
-// //     matchColumnOne.innerHTML = "in column one" + " " + el[2].innerHTML + " " + "is a matching number"  ;
-// // }
-// // if (el[0].innerHTML === el[3].innerHTML === el[6].innerHTML ) {
-// //   el[0].classList.add("highlight");
-// //   el[3].classList.add("highlight");
-// //   el[6].classList.add("highlight");
-// //   matchColumnOne.innerHTML = "in column three" + " " + el[6].innerHTML + " " + "is a matching number" ;
-// // }
-// //
-// //   if (el[1].innerHTML === el[7].innerHTML) {
-// //     el[1].classList.add("highlight");
-// //     el[7].classList.add("highlight");
-// //     matchColumnTwo.innerHTML = "in column two" + " " + el[7].innerHTML + " " + "is a matching" ;
-// //
-// //   }
-// //
-// //   if (el[1].innerHTML === el[4].innerHTML) {
-// //     el[1].classList.add("highlight");
-// //     el[4].classList.add("highlight");
-// //     matchColumnTwo.innerHTML = "in column two" + " " + el[4].innerHTML + " " + "is a matching number"  ;
-// //   }
-// //
-// //
-// //   if (el[4].innerHTML === el[7].innerHTML) {
-// //     el[4].classList.add("highlight");
-// //     el[7].classList.add("highlight");
-// //     matchColumnTwo.innerHTML =  "in column two" + " " + el[6].innerHTML + " " + "is a matching number" ;
-// //   }
-// //   if (el[1].innerHTML === el[4].innerHTML === el[7]) {
-// //     el[1].classList.add("highlight");
-// //     el[4].classList.add("highlight");
-// //     el[7].classList.add("highlight");
-// //     matchColumnTwo.innerHTML = "in column three" + " " + el[7].innerHTML + " " + "is a matching number" ;
-// //   }
-// //
-// //
-// //   if (el[2].innerHTML === el[8].innerHTML) {
-// //     el[2].classList.add("highlight");
-// //     el[8].classList.add("highlight");
-// //     matchColumnThree.innerHTML = "in column three" + " " + el[1].innerHTML + " " + "is a matching number"  ;
-// //
-// //   }
-// //
-// //   if (el[2].innerHTML === el[5].innerHTML) {
-// //     el[2].classList.add("highlight");
-// //     el[5].classList.add("highlight");
-// //     matchColumnThree.innerHTML = "in column three" + " " + el[7].innerHTML + " " + "is a matching number" ;
-// //   }
-// //
-// //
-// //   if (el[5].innerHTML === el[8].innerHTML) {
-// //     el[5].classList.add("highlight");
-// //     el[8].classList.add("highlight");
-// //     matchDiagonalsThree.innerHTML = "in column three" + " " + el[8].innerHTML + " " + "is a matching number" ;
-// //   }
-//   if (el[2].innerHTML === el[5].innerHTML === el[8].innerHTML) {
-//     el[2].classList.add("highlight");
-//     el[5].classList.add("highlight");
-//     el[8].classList.add("highlight");
-//     matchDiagonalsThree.innerHTML = "in Column three" + " " + el[8].innerHTML + " " + "is a matching number" ;
-//   }
+        matchingColumns();
+
+
 //
-// }
-//
-//
-// upDate();
-// matchingDiagonals();
-//
+var matchingDiagonals = function() {
+  var matchingDiagonalOne = document.getElementById("messageSeven");
+  var matchingDiagonalTwo = document.getElementById("messageEight");
+
+  if (el[0].innerHTML === el[8].innerHTML) {
+    el[0].classList.add("highlight");
+    el[8].classList.add("highlight");
+    matchingDiagonalOne.innerHTML = "In diagonal one" + " " + el[0].innerHTML + " " + "is a matching number";
+
+  }
+
+  if (el[0].innerHTML === el[4].innerHTML) {
+    el[0].classList.add("highlight");
+    el[4].classList.add("highlight");
+    matchingDiagonalOne.innerHTML = "In diagonal one" + " " + el[0].innerHTML + " " + "is a matching number";
+  }
+
+
+  if (el[4].innerHTML === el[8].innerHTML) {
+    el[4].classList.add("highlight");
+    el[8].classList.add("highlight");
+    matchingDiagonalOne.innerHTML = "In diagonal one" + " " + el[8].innerHTML + " " + "is a matching number"  ;
+}
+if (el[0].innerHTML === el[4].innerHTML === el[8].innerHTML ) {
+  el[0].classList.add("highlight");
+  el[4].classList.add("highlight");
+  el[8].classList.add("highlight");
+  matchingDiagonalOne.innerHTML = "In diagonal one" + " " + el[8].innerHTML + " " + "is a matching number" ;
+}
+
+  if (el[2].innerHTML === el[6].innerHTML) {
+    el[2].classList.add("highlight");
+    el[6].classList.add("highlight");
+    matchingDiagonalTwo.innerHTML = "In diagonal two" + " " + el[6].innerHTML + " " + "is a matching" ;
+
+  }
+
+  if (el[2].innerHTML === el[4].innerHTML) {
+    el[2].classList.add("highlight");
+    el[4].classList.add("highlight");
+    matchingDiagonalTwo.innerHTML = "In diagonal two" + " " + el[4].innerHTML + " " + "is a matching number"  ;
+  }
+
+
+  if (el[4].innerHTML === el[6].innerHTML) {
+    el[4].classList.add("highlight");
+    el[6].classList.add("highlight");
+    matchingDiagonalTwo.innerHTML =  "In diagonal two" + " " + el[6].innerHTML + " " + "is a matching number" ;
+  }
+  if (el[2].innerHTML === el[4].innerHTML === el[6]) {
+    el[2].classList.add("highlight");
+    el[4].classList.add("highlight");
+    el[6].classList.add("highlight");
+    matchingDiagonalTwo.innerHTML = "In diagonal three" + " " + el[6].innerHTML + " " + "is a matching number" ;
+  }
+};
+matchingDiagonals();
